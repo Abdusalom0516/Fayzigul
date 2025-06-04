b6cb0f5135cb4ceb83da8cdc920a7b194a6c3a30
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2025-06-04 17:52:56 +0500

    BASE: Create Login Screen.

--- lib/core/consts/const_img_paths.dart
+++ lib/core/consts/const_img_paths.dart
@@ -1,1 +1,4 @@
-class ConstImgPaths {}
+class ConstImgPaths {
+  String loginPlant = "assets/images/login_plant.png";
+  String plant = "assets/images/plant.png";
+}
--- lib/presentation/auth/screens/login_screen.dart
+++ lib/presentation/auth/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:plant_store/core/utils/app_state_wrapper.dart';
 import 'package:plant_store/presentation/auth/bloc/login/login_bloc.dart';
 import 'package:plant_store/presentation/auth/bloc/login/login_states.dart';
@@ -11,7 +12,28 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<LoginBloc, LoginStates>(
       builder: (context, state) => AppStateWrapper(
-        builder: (colors, texts, images) => Scaffold(),
+        builder: (colors, texts, images) => Scaffold(
+          body: Column(
+            children: [
+              // Login Top Image Section
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Image.asset(
+                      images.loginPlant,
+                      fit: BoxFit.contain,
+                      width: 375.w,
+                      height: 391.h,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(child: Column())
+            ],
+          ),
+        ),
       ),
     );
   }
