a326f374dd8845fa8b8d6515b2572e00a8f91c2b
MacBook Pro <macbookpro@MacBookPro.home>
2025-06-08 00:04:22 +0500

    BASE: Implement Navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:plant_store/core/utils/app_router.dart';
 import 'package:plant_store/firebase_options.dart';
 import 'package:plant_store/presentation/auth/bloc/login/login_bloc.dart';
 import 'package:plant_store/presentation/auth/screens/login_screen.dart';
@@ -28,6 +29,7 @@ class MyApp extends StatelessWidget {
       splitScreenMode: true,
       builder: (context, child) => MaterialApp(
         debugShowCheckedModeBanner: false,
+        navigatorKey: AppRouter.navigatorKey,
         home: MultiBlocProvider(
           providers: [
             BlocProvider(
