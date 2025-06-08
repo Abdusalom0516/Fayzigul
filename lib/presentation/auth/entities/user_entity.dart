c1a353f771caea5b6c04d1cc77cc2be55cc868f9
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2025-06-08 15:49:02 +0500

    BASE: Implement UserModel.

---
+++ lib/presentation/auth/entities/user_entity.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserEntity {
+  String uid;
+  String? displayName, email;
+  bool emailVerified, isAnonymous, phoneNumber, photoURL, refreshToken, tenanId;
+  DateTime? creationTime, lastSignInTime;
+
+  UserEntity({
+    required this.uid,
+    required this.email,
+    required this.displayName,
+    required this.emailVerified,
+    required this.isAnonymous,
+    required this.phoneNumber,
+    required this.photoURL,
+    required this.refreshToken,
+    required this.tenanId,
+    required this.creationTime,
+    required this.lastSignInTime,
+  });
+}
