bf80855e63d999b5aa460b135ac6d690ccb3b8ab
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2025-06-09 18:28:48 +0500

    BASE: Enhance Architecture Of The Home Screen.

--- lib/presentation/home/screens/home_screen.dart
+++ lib/presentation/home/screens/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:plant_store/core/common/consts/const_colors.dart';
 import 'package:plant_store/core/common/consts/const_text_styles.dart';
+import 'package:plant_store/core/common/consts/const_texts.dart';
 import 'package:plant_store/core/common/widgets/custom_sliver_height_wd.dart';
 import 'package:plant_store/core/utils/app_state_wrapper.dart';
+import 'package:plant_store/presentation/home/widgets/plants_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,16 +17,14 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: colors.ffffffff,
         body: Column(
           children: [
+            // Home Screen Top Section
             Container(
-              padding: EdgeInsets.only(
-                top: 75.r,
-              ),
               height: 318.h,
-              decoration: BoxDecoration(
-                color: colors.fff6f6f6,
-              ),
+              padding: EdgeInsets.only(top: 75.r),
+              decoration: BoxDecoration(color: colors.fff6f6f6),
               child: Stack(
                 children: [
+                  // Home Screen Top Image Section
                   Align(
                     alignment: Alignment.bottomRight,
                     child: Image.asset(
@@ -33,160 +34,129 @@ class HomeScreen extends StatelessWidget {
                       fit: BoxFit.contain,
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsetsGeometry.symmetric(horizontal: 24.w),
-                    child: Column(
-                      spacing: 11.h,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            SizedBox(
-                              width: 223.w,
-                              child: Text(
-                                texts.plantaShining,
-                                style: AppTextStyles.lato.medium(
-                                  color: colors.ff221fif,
-                                  fontSize: 24.sp,
-                                ),
-                              ),
-                            ),
-                            Transform.translate(
-                              offset: Offset(0, -15.h),
-                              child: Container(
-                                height: 48.h,
-                                width: 48.w,
-                                decoration: BoxDecoration(
-                                  color: colors.ff221fif,
-                                  shape: BoxShape.circle,
-                                ),
-                                child: Icon(
-                                  Icons.shopping_cart,
-                                  color: colors.fff6f6f6,
-                                  size: 24.r,
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                        TextButton(
-                          style: ButtonStyle(
-                            padding: WidgetStatePropertyAll(EdgeInsets.zero),
-                            overlayColor:
-                                WidgetStatePropertyAll(colors.transparent),
-                          ),
-                          onPressed: () {
-                            // log("See New Arrivals Clicked");
-                          },
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            spacing: 11.w,
-                            children: [
-                              Text(
-                                texts.seeNewArrivals,
-                                style: AppTextStyles.lato.medium(
-                                  color: colors.ff007537,
-                                  fontSize: 17.sp,
-                                ),
-                              ),
-                              Icon(
-                                Icons.arrow_forward_rounded,
-                                color: colors.ff007537,
-                                size: 24.w,
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                  // Home Screen Top Texts Section
+                  homeScreenTopTextsSection(texts, colors),
                 ],
               ),
             ),
+            // Home Screen Bottom Section
             Expanded(
                 child: CustomScrollView(
               slivers: [
                 SliverHeight(height: 25),
-                SliverPadding(
-                  padding: EdgeInsetsGeometry.symmetric(horizontal: 24.w),
-                  sliver: SliverToBoxAdapter(
-                    child: Text(
-                      texts.plants,
-                      style: AppTextStyles.lato.medium(
-                        color: colors.ff221fif,
-                        fontSize: 24.sp,
-                      ),
-                    ),
-                  ),
-                ),
+                // Home Screen Bottom Plants Title Section
+                homeScreenPlantsTitleSection(texts, colors),
                 SliverHeight(height: 9),
-                SliverPadding(
-                  padding: EdgeInsetsGeometry.symmetric(horizontal: 24.w),
-                  sliver: SliverGrid.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 155 / 217,
-                      crossAxisSpacing: 15.w,
-                      mainAxisSpacing: 15.h,
-                    ),
-                    itemBuilder: (context, index) => SizedBox(
-                      child: Column(
-                        spacing: 2.h,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: colors.fff6f6f6,
-                              borderRadius: BorderRadius.circular(8.r),
-                            ),
-                            child: Image.asset(
-                              images.plant,
-                              height: 134.h,
-                              width: 155.w,
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  overflow: TextOverflow.ellipsis,
-                                  "Spider Plant",
-                                  style: AppTextStyles.lato.medium(
-                                    color: colors.ff221fif,
-                                    fontSize: 19.sp,
-                                  ),
-                                ),
-                                Text(
-                                  overflow: TextOverflow.ellipsis,
-                                  "Indoor",
-                                  style: AppTextStyles.lato.medium(
-                                    color: colors.ffababab,
-                                    fontSize: 16.sp,
-                                  ),
-                                ),
-                                Text(
-                                  overflow: TextOverflow.ellipsis,
-                                  "\$250",
-                                  style: AppTextStyles.lato.medium(
-                                    color: colors.ff007537,
-                                    fontSize: 19.sp,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                )
+                // Home Screen Bottom Plants GridView Section
+                homeScreenPlantsGridViewSection(),
               ],
             )),
           ],
         ),
+      ),
+    );
+  }
+
+  SliverPadding homeScreenPlantsTitleSection(
+      ConstTexts texts, ConstColors colors) {
+    return SliverPadding(
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 24.w),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          texts.plants,
+          style: AppTextStyles.lato.medium(
+            color: colors.ff221fif,
+            fontSize: 24.sp,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding homeScreenTopTextsSection(ConstTexts texts, ConstColors colors) {
+    return Padding(
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 24.w),
+      child: Column(
+        spacing: 11.h,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Text
+              SizedBox(
+                width: 223.w,
+                child: Text(
+                  texts.plantaShining,
+                  style: AppTextStyles.lato.medium(
+                    color: colors.ff221fif,
+                    fontSize: 24.sp,
+                  ),
+                ),
+              ),
+              // Cart Button
+              Transform.translate(
+                offset: Offset(0, -15.h),
+                child: Container(
+                  height: 48.h,
+                  width: 48.w,
+                  decoration: BoxDecoration(
+                    color: colors.ff221fif,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(
+                    Icons.shopping_cart,
+                    color: colors.fff6f6f6,
+                    size: 24.r,
+                  ),
+                ),
+              )
+            ],
+          ),
+          // See New Arrivals Button
+          TextButton(
+            style: ButtonStyle(
+              padding: WidgetStatePropertyAll(EdgeInsets.zero),
+              overlayColor: WidgetStatePropertyAll(colors.transparent),
+            ),
+            onPressed: () {
+              // log("See New Arrivals Clicked");
+            },
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              spacing: 11.w,
+              children: [
+                Text(
+                  texts.seeNewArrivals,
+                  style: AppTextStyles.lato.medium(
+                    color: colors.ff007537,
+                    fontSize: 17.sp,
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_forward_rounded,
+                  color: colors.ff007537,
+                  size: 24.w,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  SliverPadding homeScreenPlantsGridViewSection() {
+    return SliverPadding(
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 24.w),
+      sliver: SliverGrid.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 155 / 217,
+          crossAxisSpacing: 15.w,
+          mainAxisSpacing: 15.h,
+        ),
+        itemBuilder: (context, index) => PlantCard(),
       ),
     );
   }
