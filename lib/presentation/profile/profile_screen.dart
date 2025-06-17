d81e7dc51ae65898f24fba8524831b0ae8401632
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2025-06-17 22:39:15 +0500

    BASE: Create Profile Screen.

---
+++ lib/presentation/profile/profile_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:plant_store/core/utils/app_state_wrapper.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppStateWrapper(
+      builder: (colors, texts, images) => Scaffold(
+        body: CustomScrollView(
+          slivers: [],
+        ),
+      ),
+    );
+  }
+}
