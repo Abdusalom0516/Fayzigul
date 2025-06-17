1d5639d69120a36742faf76d34e676829efa5e60
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2025-06-17 23:49:32 +0500

    BASE: Enhance Architecture In Profile Screen.

--- lib/presentation/profile/screens/profile_screen.dart
+++ lib/presentation/profile/screens/profile_screen.dart
@@ -4,6 +4,7 @@ import 'package:plant_store/core/common/consts/const_colors.dart';
 import 'package:plant_store/core/common/consts/const_text_styles.dart';
 import 'package:plant_store/core/common/widgets/custom_sliver_height_wd.dart';
 import 'package:plant_store/core/utils/app_state_wrapper.dart';
+import 'package:plant_store/presentation/profile/profile_card.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -124,46 +125,6 @@ class ProfileScreen extends StatelessWidget {
         style: AppTextStyles.lato.medium(
           color: colors.ff221fif,
           fontSize: 23.sp,
-        ),
-      ),
-    );
-  }
-}
-
-class ProfileCard extends StatelessWidget {
-  const ProfileCard({
-    super.key,
-    required this.title,
-    required this.func,
-    this.isRed = false,
-  });
-  final String title;
-  final VoidCallback func;
-  final bool isRed;
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: AppStateWrapper(
-        builder: (colors, texts, images) => TextButton(
-          onPressed: func,
-          style: ButtonStyle(
-            padding: WidgetStatePropertyAll(EdgeInsets.zero),
-            overlayColor: WidgetStatePropertyAll(colors.transparent),
-          ),
-          child: Container(
-            height: 49.h,
-            alignment: Alignment.centerLeft,
-            width: double.infinity,
-            padding: EdgeInsets.symmetric(horizontal: 48.r),
-            child: Text(
-              title,
-              style: AppTextStyles.lato.medium(
-                color: isRed ? colors.ffff0000 : colors.ff221fif,
-                fontSize: 19.sp,
-              ),
-            ),
-          ),
         ),
       ),
     );
