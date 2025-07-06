085d6a1c53be7f7e2e8ca2f256778039a89284d8
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2025-07-06 10:01:55 +0500

    BASE: Implement Events.

--- lib/presentation/cart/bloc/cart_bloc.dart
+++ lib/presentation/cart/bloc/cart_bloc.dart
@@ -1,13 +1,31 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:plant_store/presentation/cart/bloc/cart_bloc_events.dart';
 import 'package:plant_store/presentation/cart/bloc/cart_bloc_states.dart';
+import 'package:plant_store/presentation/cart/models/cart_product_model.dart';
 
 class CartBloc extends Bloc<CartBlocEvents, CartBlocStates> {
   CartBloc() : super(CartBlocStates(cartProductsList: [])) {
-
     // Event Handling for OnAddProductEvent Event
     on<OnAddProductToCart>(
-      (event, emit) {},
+      (event, emit) {
+        // Cart Product List
+        var cartProductList =
+            List<CartProductModel>.from(state.cartProductsList);
+
+        for (int i = 0; i < cartProductList.length; i++) {
+          if (event.product.id == cartProductList[i].product.id) {
+            cartProductList[i] = cartProductList[i].copyWith(
+                productQuantity:
+                    event.quantity + cartProductList[i].productQuantity);
+
+            emit(CartBlocStates(cartProductsList: cartProductList));
+            return;
+          }
+        }
+
+        cartProductList.add(CartProductModel(
+            product: event.product, productQuantity: event.quantity));
+      },
     );
 
     // Event Handling for OnMinusProductFromCart Event
--- lib/presentation/cart/entities/cart_product_entity.dart
+++ lib/presentation/cart/entities/cart_product_entity.dart
@@ -1,13 +1,12 @@
 import 'package:equatable/equatable.dart';
 import 'package:plant_store/presentation/home/models/product_model.dart';
 
-class CartEntity extends Equatable{
+class CartEntity extends Equatable {
   final ProductModel product;
   final int productQuantity;
 
   const CartEntity({required this.product, required this.productQuantity});
 
   @override
-  List<Object?> get props => [product.id, productQuantity];
+  List<Object?> get props => [product.id];
 }
-
