0a21f4df76f27db2da611c4340634545ed406c90
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2025-07-14 16:43:01 +0500

    BASE: Create CartLocalDataSource Class.

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox('searchHistory');
+  await Hive.openBox('cart');
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
