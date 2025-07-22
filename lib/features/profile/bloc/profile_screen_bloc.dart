0817076550b8c163f508cbd1e116bd717b5fb5c5
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2025-07-22 23:49:08 +0500

    BASE: Implement User Remove Logic In Logout Section.

--- lib/features/profile/bloc/profile_screen_bloc.dart
+++ lib/features/profile/bloc/profile_screen_bloc.dart
@@ -5,6 +5,7 @@ import 'package:plant_store/core/utils/app_router.dart';
 import 'package:plant_store/core/utils/toastification.dart';
 import 'package:plant_store/features/auth/data/datasources/auth_local_data_source.dart';
 import 'package:plant_store/features/auth/data/repositories/auth_repository_implementation.dart';
+import 'package:plant_store/features/auth/domain/usecases/remove_user_usecase.dart';
 import 'package:plant_store/features/auth/presentation/screens/login_screen.dart';
 import 'package:plant_store/features/profile/bloc/profile_screen_bloc_events.dart';
 import 'package:plant_store/features/profile/bloc/profile_scren_bloc_states.dart';
@@ -45,13 +46,14 @@ class ProfileScreenBloc extends Bloc<ProfileScreenEvents, ProfileScreenStates> {
       AuthRepositoryImplementation repository =
           AuthRepositoryImplementation(localDataSource: AuthLocalDataSource());
 
-      final removeUserUseCase = 
+      final removeUserUseCase = RemoveUserUsecase(repository: repository);
       emit(ProfileScreenLoadingState());
       await Future.delayed(const Duration(seconds: 1));
 
       try {
         final auth = FirebaseAuth.instance;
         await auth.signOut();
+        await removeUserUseCase();
 
         AppRouter.open(LoginScreen());
       } catch (e) {
